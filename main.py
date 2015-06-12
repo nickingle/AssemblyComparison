cfe6e18d4d348743bc35d0fcd95ce86adccfaa5b
Nick Ingle <nickandingle@gmail.com>
2015-06-12 16:42:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ def graphAllPMs(pmList, names, n):
 		axes[y,0].hist(pm, range = (min(pm), 100))
 		name = list(names)[y]
 		title = '{0} : minia to velvet'.format(name)
-		axes[y,0]set_title(title, fontsize=10)
+		axes[y,0].set_title(title, fontsize=10)
 		pm = list(pmList)[y+1]
 		axes[y,1].hist(pm, range = (min(pm), 100))
 		title = '{0} : velvet to minia'.format(name)
