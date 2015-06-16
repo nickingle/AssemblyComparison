ec8d1b5167a72c85cdd7e9771d00b5b4172296ae
Nick Ingle <nickandingle@gmail.com>
2015-06-16 12:12:03 -0700

    Created cMapped.py

---
+++ cMapped.py
@@ -0,0 +1,63 @@
+import sys
+import pysam
+import string
+import re
+import numpy as np
+import matplotlib
+import matplotlib as plt
+
+reference_id - returns index of reference sequence in the reference dictionary
+lengths - tuple of the lengths of the reference sequences(same order as the reference files)
+
+def createDict(sfile):
+	refLengths = ()
+	refLengths = refLengths + sfile.lengths
+	
+	LengthDict = {}
+	
+	for readseg in sfile.fetch():
+		index = readseg.reference_id
+		lengthRef = refLengths[index] # returns the length of the corresponding reference seq that that the read maps to
+		if LengthDict.hasKey(lengthRef):
+			count = LengthDict[lengthRef]
+			LengthDict[lengthRef] = count + 1
+		else:
+			count = 1
+			LengthDict[lengthRef] = count
+	
+	return LengthDict
+
+def plotGraph(d):
+	x = []
+	y = []
+	
+	x = d.keys()
+	y = d.values()
+	
+	plt.plot(x,y,'.')
+	plt.title('# of Minia Contigs Mapped to Velvet Refs', fontsize = 25)
+	plt.ylabel('# of Minia Contigs Mapped to Reference Seq', fontsize = 20)
+	plt.yticks(fontsize = 15)
+	plt.xlabel('Length of Velvet Sequence', fontsize = 20)
+	plt.xticks(fontsize = 15)
+	plt.savefig('Contigs_Mapped_SP.png')
+
+if __name__ == "__main__":
+	input("Press Enter to display results...")
+	samfile = samfile = pysam.AlignmentFile(sys.argv[1], "r")
+	d = createDict(samfile)
+	samfile.close()
+	plotGraph(d)
+	
+	
+	
+	
+	
+				
+		
+		
+		
+	
+	
+	
+	
