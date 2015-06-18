42475cc3113b1d7811e3fe19f68f0771763f8c16
Nick Ingle <nickandingle@gmail.com>
2015-06-18 12:35:11 -0700

    Added scatter graph for velvet to minia

--- cMapped.py
+++ cMapped.py
@@ -27,28 +27,42 @@ def createDict(sfile):
 	
 	return LengthDict
 
-def plotGraph(d):
+def plotGraph(d1,d2):
 	x = []
 	y = []
 	
-	x = d.keys()
-	y = d.values()
+	x = d1.keys()
+	y = d1.values()
 	
 	plt.plot([float(v) for v in x],[float(k) for k in y],'.')
 	plt.title('# of Minia Contigs Mapped to Velvet Refs', fontsize = 25)
-	plt.ylabel('# of Minia Contigs Mapped to Reference Seq', fontsize = 10)
+	plt.ylabel('# of Minia Contigs Mapped to Reference Seq', fontsize = 13)
 	plt.yticks(fontsize = 15)
 	plt.xlabel('Length of Velvet Sequence', fontsize = 20)
 	plt.xticks(fontsize = 15)
 	plt.savefig('Contigs_Mapped_SP.png')
 	
+	x = d2.keys()
+	y = d2.values()
+	
+	plt.plot([float(v) for v in x)],[float(k) for k in y], '.')
+	plt.title('# of Velvet Contigs Mapped to Minia Refs', fontsize = 20)
+	plt.ylabel('# of Velvet Contigs Mapped to Reference Seq', fontsize = 13)
+	plt.yticks(fontsize = 15)
+	plt.xlabel('Length of Minia Sequence', fontsize = 15)
+	plt.xticks(fontsize = 15)
+	plt.savefig('Contigs_Mapped_VtoM.png')
+	
 
 if __name__ == "__main__":
 	input("Press Enter to display results...")
-	samfile = samfile = pysam.AlignmentFile(sys.argv[1], "r")
-	d = createDict(samfile)
-	samfile.close()
-	plotGraph(d)
+	samfile1 = pysam.AlignmentFile(sys.argv[1], "r")
+	samfile2 = pysam.AlignmentFile(sys.argv[2], "r")
+	d1 = createDict(samfile1)
+	d2 = createDict(samfile2)
+	samfile1.close()
+	samfile2.close()
+	plotGraph(d1,d2)
 	
 	
 	
