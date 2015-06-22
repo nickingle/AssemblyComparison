b9d69e5c6016c0e13bd96ac147b23c2e1ff3568e
Nick Ingle <nickandingle@gmail.com>
2015-06-22 12:59:35 -0700

    Updated

--- cMapped.py
+++ cMapped.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
 	cmList = list()
 	names = list()
 	
-	for x in range(2,(inputnum+2)):
+	for x in range(2,(inputnum+1)):
 		counter += 1
 		if counter == 3:
 			names.append(str(sys.argv[x]))
